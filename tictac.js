6af32dbe9bfa4c7eb6448d67f4c0061dffd220f6
sameh-dh <samehdh44@gmail.com>
2022-05-06 14:59:41 +0300

    player can change background

--- tictac.js
+++ tictac.js
@@ -1,67 +1,75 @@
-var move=1;
-var play=true;
-$('table tr td').click(function(){
-    if($(this).text()== "" && play){
-        if( (move%2)==1){
-            $(this).append("X");
-            $(this).css('color','#61892f');
-
-        } else {
-            $(this).append("O");
-            $(this).css('color', '#e85a4f');
-            
-        }
-        move++;
-        if (findWinner() != -1 && findWinner()!= ""){
-            if(findWinner()=="X"){
-                $('body').append('<div class="winner"><span>Winner</span>X</div>');
-                $('body').append('<button onclick="location.reload()">Play Again</button>');
-                $('.winner').css('background-color', '#61892f');
-                $('button').css('color','#61892f');
+$(document).ready(function(){
+    var move=1;
+    var play=true;
+    $('table tr td').click(function(){
+        if($(this).text()== "" && play){
+            if( (move%2)==1){
+                $(this).append("X");
+                $(this).css('color','#61892f');
+    
             } else {
-                $('body').append("<div class='winner'><span>Winner</span>O</div>");
-                $('body').append('<button onclick="location.reload()">Play Again</button>');
-                $('.winner').css('background-color', '#e85a4f');
-                $('button').css('color','#e85a4f');
-
+                $(this).append("O");
+                $(this).css('color', '#e85a4f');
+                
             }
-            play = false;
+            move++;
+            if (findWinner() != -1 && findWinner()!= ""){
+                if(findWinner()=="X"){
+                    $('body').append('<div class="winner"><span>Winner</span>X</div>');
+                    $('body').append('<button class="playAgain" onclick="location.reload()">Play Again</button></br>');
+                    $('.winner').css({'background-color':'#B388EB'});
+                    $('button').css('color','#61892f');
+                } else {
+                    $('body').append("<div class='winner'><span>Winner </span> O </div>");
+                    $('body').append('<button class="playAgain" onclick="location.reload()">Play Again</button></br>');
+                    $('.winner').css({'background-color':'#B388EB'});
+                    $('button').css('color','#e85a4f');
+    
+                }
+                play = false;
+            }
+    
         }
-
-    }
-});
-function findWinner(){
-    sp1 = $('table tr:nth-child(1) td:nth-child(1)').text();
-    sp2 = $('table tr:nth-child(1) td:nth-child(2)').text();
-    sp3 = $('table tr:nth-child(1) td:nth-child(3)').text();
-    sp4 = $('table tr:nth-child(2) td:nth-child(1)').text();
-    sp5 = $('table tr:nth-child(2) td:nth-child(2)').text();
-    sp6 = $('table tr:nth-child(2) td:nth-child(3)').text();
-    sp7 = $('table tr:nth-child(3) td:nth-child(1)').text();
-    sp8 = $('table tr:nth-child(3) td:nth-child(2)').text();
-    sp9 = $('table tr:nth-child(3) td:nth-child(3)').text();
-    if((sp1 == sp2) && (sp2 == sp3)) {
-        return sp3;
-    } else if ((sp4 == sp5) && (sp5 == sp6)){
-        return sp6;
-    } else if ((sp7 == sp8) && (sp8 == sp9)){
+    });
+    function findWinner(){
+        sp1 = $('table tr:nth-child(1) td:nth-child(1)').text();
+        sp2 = $('table tr:nth-child(1) td:nth-child(2)').text();
+        sp3 = $('table tr:nth-child(1) td:nth-child(3)').text();
+        sp4 = $('table tr:nth-child(2) td:nth-child(1)').text();
+        sp5 = $('table tr:nth-child(2) td:nth-child(2)').text();
+        sp6 = $('table tr:nth-child(2) td:nth-child(3)').text();
+        sp7 = $('table tr:nth-child(3) td:nth-child(1)').text();
+        sp8 = $('table tr:nth-child(3) td:nth-child(2)').text();
+        sp9 = $('table tr:nth-child(3) td:nth-child(3)').text();
+        if((sp1 == sp2) && (sp2 == sp3)) {
+            return sp3;
+        } else if ((sp4 == sp5) && (sp5 == sp6)){
+            return sp6;
+        } else if ((sp7 == sp8) && (sp8 == sp9)){
+            return sp9;
+        }
+        else if ((sp1 == sp4) && (sp4 == sp7)){
+            return sp7;
+    }  else if ((sp2 == sp5) && (sp5 == sp8)){
+        return sp8;
+    } else if ((sp3 == sp6) && (sp6 == sp9)){
         return sp9;
-    }
-    else if ((sp1 == sp4) && (sp4 == sp7)){
+    
+    } 
+    else if ((sp1 == sp5) && (sp5 == sp9)){
+        return sp9;
+    
+    } 
+    else if ( (sp3 == sp5) && (sp5 == sp7)){
         return sp7;
-}  else if ((sp2 == sp5) && (sp5 == sp8)){
-    return sp8;
-} else if ((sp3 == sp6) && (sp6 == sp9)){
-    return sp9;
-
-} 
-else if ((sp1 == sp5) && (sp5 == sp9)){
-    return sp9;
-
-} 
-else if ( (sp3 == sp5) && (sp5 == sp7)){
-    return sp7;
-}
-
-return -1;
-}
+    }
+    
+    return -1;
+    }
+    $('#colorButton').on('click',function(){
+        var color = $('#colorInputColor').val()
+        $('table tr td').css('background-color',color)
+    })
+    
+     
+    });
